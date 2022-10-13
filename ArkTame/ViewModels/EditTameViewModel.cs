f8d2d04067dbf55b9b842668a8e63e6ccb44ac0e
Stefan Henseler <stefan.henseler@adesso.de>
2022-10-13 17:14:31 +0200

    Done

--- ArkTame/ViewModels/EditTameViewModel.cs
+++ ArkTame/ViewModels/EditTameViewModel.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using AdonisUI.Controls;
 using ArkTame.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MessageBox = AdonisUI.Controls.MessageBox;
+using MessageBoxButton = AdonisUI.Controls.MessageBoxButton;
+using MessageBoxImage = AdonisUI.Controls.MessageBoxImage;
 
 namespace ArkTame.ViewModels;
 
-public class EditTameViewModel
+public partial class EditTameViewModel : ObservableObject
 {
-    // public bool IsMale => SelectedSex == Sex.Male;
+    public bool IsMale => SelectedSex == Sex.Male;
 
-    // public bool IsFemale => SelectedSex == Sex.Female;
+    public bool IsFemale => SelectedSex == Sex.Female;
 
     public static EditTameViewModel Design { get; } = new(null);
 
@@ -22,20 +25,55 @@ public class EditTameViewModel
         AvailableTames = tameService?.GetTameInfosFromFile();
     }
 
+    [ObservableProperty]
+    private string? _name;
+
+    [ObservableProperty]
+    private TameInfo? _selectedTame;
+
+    [ObservableProperty]
+    private Sex? _selectedSex;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Level))]
+    private int? _health;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Level))]
+    private int? _stamina;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Level))]
+    private int? _oxygen;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Level))]
+    private int? _food;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Level))]
+    private int? _weight;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Level))]
+    private int? _meleeDamage;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(Level))]
+    private int? _movementSpeed;
+
     private int GetLevel()
     {
-        // var result = 1 +
-        //     (Health ?? 0) +
-        //     (Stamina ?? 0) +
-        //     (Oxygen ?? 0) +
-        //     (Food ?? 0) +
-        //     (Weight ?? 0) +
-        //     (MeleeDamage ?? 0) +
-        //     (MovementSpeed ?? 0);
-        //
-        // return result;
+        var result = 1 +
+            (Health ?? 0) +
+            (Stamina ?? 0) +
+            (Oxygen ?? 0) +
+            (Food ?? 0) +
+            (Weight ?? 0) +
+            (MeleeDamage ?? 0) +
+            (MovementSpeed ?? 0);
 
-        return 0;
+        return result;
     }
 
     private void SetSex(Sex sex)
